54fcf0a236faed849077a7c19ad81606e86abda9
johnsonwyo <johnsonwyo@gmail.com>
2023-07-12 20:37:18 -0500

    asdf

---
+++ src/test/java/composite/TestParserMultiplication.java
@@ -0,0 +1,25 @@
+package composite;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for parser
+ */
+public class TestParserMultiplication {
+
+    @Test
+    void allOperations() {
+        Parser mainParser = new Expression("2+7*4-1/1");
+        double outcome = mainParser.eval();
+        assertEquals(29.0, outcome);
+    }
+
+    @Test
+    void multiplication() {
+        Parser mainParser = new Expression("1*2*3*4*5");
+        double outcome = mainParser.eval();
+        assertEquals(1, outcome);
+    }
+}
